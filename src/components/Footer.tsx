b9d75fa74eb32b2fb4dbc5f7419fdb5a0b105726
uellenberg <JonahUellenberg@hotmail.com>
2025-10-19 10:54:47 -0700

    Fix build

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <footer className="w-full p-4 bg-black text-center border-t border-gray-200 mt-auto">
-            <p className="text-white">DubHacks'25</p>
+            <p className="text-white">DubHacks&apos;25</p>
         </footer>
     );
 };
