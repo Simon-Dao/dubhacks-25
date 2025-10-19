a2db9d9ea7603cd8f221a653b33378d27b33d065
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-10-19 06:22:36 -0700

    didnt get database to work.

--- src/components/workspace/DraggableClothing.tsx
+++ src/components/workspace/DraggableClothing.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useDraggable } from "@dnd-kit/core";
 
 import Image from "next/image";
+import { useLocalState } from "@/app/state/state";
 
 interface DraggableClothingProps {
     id: string;
@@ -11,25 +12,44 @@ interface DraggableClothingProps {
 }
 
 const DraggableClothing: React.FC<DraggableClothingProps> = ({ id, url }) => {
+    const [saved, setSaved] = React.useState(false);
+
     const { attributes, listeners, setNodeRef } = useDraggable({
         id: id,
         data: { url },
     });
 
+    async function onclick() {
+    }
+
     return (
-        <div
-            ref={setNodeRef}
-            {...listeners}
-            {...attributes}
-            className="cursor-move border border-gray-300 rounded-lg overflow-hidden"
-        >
-            <Image
-                src={url}
-                alt="Generated clothing"
-                width={150}
-                height={150}
-                className="w-full h-auto"
-            />
+        <div>
+            <div
+                ref={setNodeRef}
+                {...listeners}
+                {...attributes}
+                className="cursor-move border border-gray-300 rounded-lg overflow-hidden"
+            >
+                <Image
+                    src={url}
+                    alt="Generated clothing"
+                    width={150}
+                    height={150}
+                    className="w-full h-auto"
+                />
+            </div>
+
+            <button
+                className={
+                    saved
+                        ? "bg-green-600 "
+                        : "bg-blue-600" +
+                          " cursor-pointer p-1 mt-1 rounded-sm text-white"
+                }
+                onClick={onclick}
+            >
+                {saved ? "saved" : "save"}
+            </button>
         </div>
     );
 };
