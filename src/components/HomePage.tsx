68933e788e2136e53c0348ac7130704b7069ef51
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-10-18 21:10:33 -0700

    finished homepage and fixed heaader redirects

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+export default function HomePage() {
+
+    return (
+        <div
+            className="flex min-h-screen flex-col bg-cover"
+            style={{ backgroundImage: "url('/mural.png')" }}
+        >
+            <Header dark={true} />
+
+            <main className="flex flex-grow flex-col items-center justify-center p-8">
+                <Link href="/workspace" className="">
+                    <button className="bg-[#1C1C1C] px-20 py-6 rounded-lg text-white flex justify-center items-center flex-col cursor-pointer hover:bg-gray-800 transition">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={2}
+                            stroke="currentColor"
+                            className="size-6"
+                            style={{ width: 60, height: 60 }}
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                            />
+                        </svg>
+                        <h2>Create</h2>
+                    </button>
+                </Link>
+            </main>
+
+            <Footer />
+        </div>
+    );
+}
